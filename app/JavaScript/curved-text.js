056f51e3985cfd9a63358d64d2e1280d605fd9b1
ty025652 <68066001+ty025652@users.noreply.github.com>
2023-09-20 03:05:47 +0800

    Add files via upload

--- app/JavaScript/curved-text.js
+++ app/JavaScript/curved-text.js
@@ -3,8 +3,25 @@ new CircleType(document.getElementById("curved-text"));
 let menu = document.querySelector(".menu");
 let nav = document.querySelector("#nav");
 let body = document.querySelector("body");
+let curvedText = document.querySelector("#curved-text")
+let closeBtn = document.querySelector(".circle-container span")
+
+let menuOpen = false
+
+
 menu.addEventListener("click", function () {
+
+  if (!menuOpen) {
   nav.style.top = "0";
-  menu.style.display = "none";
-  body.style.overflowY = 'hidden';
+  curvedText.style.display = 'none'
+  menuOpen = true
+  }
+  else{
+    nav.style.top = "-100%";
+    curvedText.style.display = "flex";
+    menuOpen = false
+  }
 });
+
+
+
