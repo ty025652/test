5c9fa7ad2d576e8da89354c1dd44c05cebc72357
ty025652 <68066001+ty025652@users.noreply.github.com>
2023-11-01 22:06:15 +0800

    Add files via upload

--- sideProject/what the fuck/js/app.js
+++ sideProject/what the fuck/js/app.js
@@ -1,13 +1,16 @@
 let menuButton = document.querySelector('#menu')
 let menuLists = document.querySelector('#nav')
+let body = document.querySelector('body')
 let menuIsOpen = false
 
 menuButton.addEventListener('click', function () {
     if (!menuIsOpen) {
         menuLists.classList.add('nav--open')
+        body.classList.add('fixed-screen')
         menuIsOpen = true
     } else {
         menuLists.classList.remove('nav--open')
+        body.classList.remove('fixed-screen')
         menuIsOpen = false
     }
 })
